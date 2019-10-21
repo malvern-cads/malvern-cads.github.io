40b3a6fed1dfad08eb504b58791236838cbe3b4a
Jake Walker <hi@jakew.me>
2019-10-21 08:09:44 +0100

    wip

--- themes/cads/scripts/gitinfo.js
+++ themes/cads/scripts/gitinfo.js
@@ -1,5 +1,5 @@
 const git = require("git-rev-sync");
 
 hexo.extend.helper.register("gitinfo", function() {
-  return `Version ${git.short()} (${git.message().split("\n")[0]})`;
+  return `Version ${git.short()} (${git.message()})`;
 });
